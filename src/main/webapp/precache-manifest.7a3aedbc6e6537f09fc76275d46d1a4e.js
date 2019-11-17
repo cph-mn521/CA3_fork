b6c9b21f6478d1d16b29d4bd00e58d27830a7285
cph-mn521 <43202726+cph-mn521@users.noreply.github.com>
2019-11-17 21:35:53 +0100

    react upd

---
+++ src/main/webapp/precache-manifest.7a3aedbc6e6537f09fc76275d46d1a4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac92025dbee16b03c4eb",
+    "url": "/ca3/static/css/main.92f29963.chunk.css"
+  },
+  {
+    "revision": "ac92025dbee16b03c4eb",
+    "url": "/ca3/static/js/main.9c51e8de.chunk.js"
+  },
+  {
+    "revision": "7d8ad7b1c7f0fa0b420d",
+    "url": "/ca3/static/js/runtime~main.22e0c177.js"
+  },
+  {
+    "revision": "6ae27261f93a26a49b80",
+    "url": "/ca3/static/js/2.d35195a6.chunk.js"
+  },
+  {
+    "revision": "932c48137aeab8cf454996bb441dddf0",
+    "url": "/ca3/index.html"
+  }
+];
